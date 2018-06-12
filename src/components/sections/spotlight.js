e81ac613f1b91e5aaaa7d8f6e375b7182a5f8081
Guillaume Ader <GAder@esriuk.com>
2018-06-12 16:35:52 +0100

    update

--- src/components/sections/spotlight.js
+++ src/components/sections/spotlight.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import css from '../../config/style.json'
 
-const item = (props) => (
-    <section style={{ backgroundColor: css.style.secBg }}>
+const item = (props, i) => (
+    <section key={i} style={{ backgroundColor: css.style.secBg }}>
         <a href="generic.html" className="image">
             <img
                 src="images/pic08.jpg"
@@ -29,13 +29,41 @@ const item = (props) => (
 
 const Spotlight = (props) => {
 
-    const build = (data) => (
-        <div>HELLO {data.title}</div>
-    )
-
-    for (var val in props) {
-        return (build(props[val]))
+    const build = (e, i) => {
+        const display = (i % 2 == 0) ? 'top center' : 'center center';
+        const bg = (i % 2 == 0) ? css.style.secBg : css.style.mainBg;
+        return (
+            <section key={i} style={{ backgroundColor: bg }}>
+                <a href="generic.html" className="image">
+                    <img
+                        src="images/pic08.jpg"
+                        alt=""
+                        data-position={display}
+                    />
+                </a>
+                <div className="content">
+                    <div className="inner">
+                        <header className="major">
+                            <h3>{e.title}</h3>
+                        </header>
+                        <p>{e.text}</p>
+                        <ul className="actions">
+                            <li>
+                                <a href="generic.html" className="button">Learn more</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        )
     }
+
+    let list = [];
+    for (var val in props) {
+        list.push(props[val])
+    }
+
+    return list.map((e, i) => <div className="spotlights">{build(e, i)}</div>)
 
 }
 export default Spotlight; 
