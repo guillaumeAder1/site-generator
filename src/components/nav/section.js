4eccd0f829215a37039f036ef3b49f87eedf0885
Guillaume Ader <GAder@esriuk.com>
2018-06-15 14:26:52 +0100

    use Route

--- src/components/nav/section.js
+++ src/components/nav/section.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import config from '../../config/conf.json'
 import Main from '../main'
 
@@ -45,6 +45,7 @@ const Def = () => (
 
 const Section = () => (
     <React.Fragment>
+
         {
             config.routes.map((e, i) => <Route exact
                 key={i}
@@ -53,9 +54,11 @@ const Section = () => (
                 render={(props) => <Main {...props} config={e.component} />} />
             )
         }
+
         {/* <Route exact path="/" component={Def} />
         <Route exact path="/my-about" component={About} />
         <Route exact path="/my-home" component={Home} /> */}
+
     </React.Fragment >
 )
 
